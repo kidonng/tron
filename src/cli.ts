ca5f169210a5435e2968c5bd4f9c601900018f37
Kid <hi@xuann.wang>
2025-07-18 14:26:47 +0800

    Fix main module resolve

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,10 @@
 #!/usr/bin/env node
 import { execFileSync } from 'node:child_process'
-import { dirname, resolve } from 'node:path'
 import { argv } from 'node:process'
 import electron from 'electron'
 
 execFileSync(
     electron as unknown as string,
-    [resolve(dirname(argv[1]), 'main.js'), ...argv.slice(2)],
+    [require.resolve('./main.js'), ...argv.slice(2)],
     { stdio: 'inherit' },
 )
